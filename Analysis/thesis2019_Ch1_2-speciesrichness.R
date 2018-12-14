d95261773fe697a445effa2e1caff9cd4c725083
Justin <jtpriest@alaska.edu>
2018-12-13 17:19:30 -0900

    updated species richness script

--- Analysis/thesis2019_Ch1_2-speciesrichness.R
+++ Analysis/thesis2019_Ch1_2-speciesrichness.R
@@ -8,6 +8,7 @@
 
 library(ggplot2)
 library(viridis)
+library(MuMIn)
 
 source("Analysis/thesis2019_Ch1_1-import&cleanup.R")
 
@@ -18,21 +19,70 @@ source("Analysis/thesis2019_Ch1_1-import&cleanup.R")
 spp_richness <- allcatch %>% filter(Species != "HYCS" & Species != "UNKN" & Station != 231) %>% group_by(Year) %>% 
   summarise(spp_yr = n_distinct(Species)) 
 
-#summary(lm(spp_yr ~ Year, data = spp_richness))
 spp_richness <- spp_richness %>% left_join(pru.env.ann %>% dplyr::select(-Station) %>% 
                                              mutate(Year = as.numeric(Year) + 2000) %>%
                                              group_by(Year) %>% summarise_all(mean), 
                                            by = c("Year" = "Year"))
 
-summary(lm(spp_yr ~ Year + annwindspeed_kph + annwinddir + anndisch_cfs + annsal_ppt + anntemp_c + annwinddir_ew, data = spp_richness))
-
-ggplot(data=spp_richness, aes(x=anntemp_c, y = spp_yr, color = Year)) + 
-  geom_point(cex = 3) + geom_smooth(method = "lm", se=FALSE) +
-  scale_color_viridis() + theme_bw() +
-  geom_text(label = spp_richness$Year, nudge_y = 0.25, size =4)
+#Set up standardized variables just in case to use later
+spp_richness.std <- as.data.frame(scale(spp_richness)) #scale to mu=0, sd=1
+spp_richness.std <- spp_richness.std %>% dplyr::select(-annwinddir)
 
 
+# Quick EDA
+summary(lm(spp_yr ~ Year + annwindspeed_kph + annwinddir + anndisch_cfs + 
+             annsal_ppt + anntemp_c + annwinddir_ew, data = spp_richness))
 
-summary(lm(spp_yr ~ Year + annsal_ppt - 1, data = spp_richness))
+plotenv <- function(env_var){
+  env_var1 <- rlang::sym(env_var)
+  
+  annotateloc <- min(spp_richness %>% dplyr::select(env_var)) +
+          (max(spp_richness %>% dplyr::select(env_var)) - min(spp_richness %>% dplyr::select(env_var)))/5
+  pval <- summary(lm(paste0("spp_yr ~ ", env_var), data = spp_richness))$coef[2,4]
+  r2 <- summary(lm(paste0("spp_yr ~ ", env_var), data = spp_richness))$adj.r.squared
+
+  p <- ggplot(data = spp_richness, aes(x = !! env_var1, y = spp_yr, color = Year)) +
+    geom_point(cex = 3) +
+    geom_smooth(method = "lm", se=FALSE) +
+    scale_color_viridis() + theme_bw() +
+    geom_text(label = spp_richness$Year, nudge_y = 0.25, size = 4) +
+    annotate("text", x = annotateloc, y = 23, label = paste0("p-value is: ", round(pval,3))) + 
+    annotate("text", x = annotateloc, y = 23.5, label = paste0("Adj R^2 is: ", round(r2,2))) 
+  return(p)
+}
+
+plotenv("Year")
+plotenv("annwindspeed_kph")
+plotenv("anndisch_cfs")
+plotenv("annsal_ppt")
+plotenv("anntemp_c")
+plotenv("annwinddir_ew")
+
+
+# SUMMARY: Individually, only Year is significant, with temp and salinity marginal. 
+# So we conclude that species richness is significantly increasing over time
+summary(lm(spp_yr ~ Year, data = spp_richness)) 
+
+
+
+
+# Now let's do some model selection to best explain the results that we see 
+options(na.action = "na.fail")
+fullmodel <- lm(spp_yr ~ Year + annwindspeed_kph + anndisch_cfs + 
+        annsal_ppt + anntemp_c + annwinddir_ew, data = spp_richness)
+fullmodel1 <- lm(spp_yr ~ Year + annwindspeed_kph + anndisch_cfs + 
+                  annsal_ppt + anntemp_c + Year:anntemp_c, data = spp_richness)
+dredge(fullmodel1)
+
+par(mar = c(3,5,6,4))
+plot(dredge(fullmodel1), labAsExpr = TRUE)
+subset(dredge(fullmodel1), delta < 4)
+
+# SUMMARY: The top model is salinity + Year, though just year works well, and
+# temp + wind speed is decent. 
+
+
+
+
 
 
--- Analysis/thesis2019_Ch1_3-pelagic.R
+++ Analysis/thesis2019_Ch1_3-pelagic.R
@@ -1,7 +1,30 @@
+# PELAGIC VS BENTHIC 
+# shelving this for now because it doesn't seem interesting
+
+
+source("Analysis/thesis2019_Ch1_1-import&cleanup.R")
 
 
 
-# Now it's new
+marinespp <- c("ARCD", "ARFL", "CAPE", "FHSC", "ELPT", "KPGL", "KPSF", "LIPA", "PCHG", "RBSM", 
+               "RKGL", "SFCD", "SHSC", "SLEB", "WOLF", "WSGL")
+freshspp <- c("ARCH", "BRBT", "GRAY", "LNSK", "SLSC")
+anadspp <- c("ARCS", "BDWF", "BRCS", "CHUM", "DLVN", "HBWF", "HYCS", "LSCS", "NNSB", "PINK", "RDWF", "SOCK", "THSB")
+
+spplookup <- spplookup %>% mutate(marine_fresh = if_else(Species %in% marinespp, "marine", 
+                                                         if_else(Species %in% freshspp, "freshwater", 
+                                                                 if_else(Species %in% anadspp, "anadromous", "NA"))))
 
 
-# I'm writing this on github.com
+spplookup <- spplookup %>% mutate(pelagic_benth = if_else(familygroup == "Cottidae" | familygroup == "Pleuronectidae" | 
+                                                          familygroup == "Other", "benthic", "pelagic"), 
+                     pelagic_benth = replace(pelagic_benth, Species == "NNSB" | Species == "THSB" | Species == "KPGL" |
+                                             Species == "RKGL" | Species == "WSGL", "pelagic")) 
+  
+
+temp <- left_join(allcatch, spplookup %>% dplyr::select(-commonname, - familygroup), 
+          by = c("Species" = "Species")) %>% 
+  group_by(Year, pelagic_benth) %>% summarise(anncatch = sum(totcount))
+
+
+ggplot(data = temp, aes(x=Year, y = anncatch, color = pelagic_benth)) + geom_line()
